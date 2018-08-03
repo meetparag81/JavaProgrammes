3b31bf6307986b86899b0ebbd0002b6c00555621
paragb <paragborawake81@gmail.com>
2018-08-03 16:19:27 +0530

    Diamond Programme is completed

--- JavaProgramme/src/main/java/JavaProgramme/Diamond.java
+++ JavaProgramme/src/main/java/JavaProgramme/Diamond.java
@@ -16,29 +16,33 @@ public class Diamond
 	{
 		for(int i =1;i<=5;i++)// used for No of rows
 		{
-			for(int k=3;k>=i;k--)
+			for(int k=4;k>=i;k--)
 			{
 				System.out.print(" ");
 			}
 			for(int j=1;j<=i;j++) // used to print * in columns, column is depends on rows
 			{
-				if(i%2!=0)
 				System.out.print("*" + " ");
+				/*if(i%2!=0)
+				{
+				System.out.print("*" + " ");
+				}*/
 			}
 			System.out.println("");// used to go on second line
 		}
-		for(int m=4;m>0;m--)// for next rows are 4-7,9,
+		for(int m=0;m<4;m++)// for next rows are 4-7,9,
 		{
-			for(int l=1;l<=m;l++)
+			for(int l=0;l<=m;l++)
 			{
 				System.out.print( " ");
 			}
-			for(int n=3;n<=m;n++)
+			for(int n=4;n>m;n--)
 			{
-				if(m%2!=0)
+				System.out.print( "*"+ " ");
+				/*if(m%2!=0)
 				{
 				System.out.print( " "+ "*");
-				}
+				}*/
 			}
 				System.out.println( "");
 			}
