1f772d573a19a23e75791314bc7dd2364f52998c
paragb <paragborawake81@gmail.com>
2018-08-06 15:41:19 +0530

    new changes

--- JavaProgramme/src/main/java/JavaInterviewProgrammes/FindDuplicatewordsfromString.java
+++ JavaProgramme/src/main/java/JavaInterviewProgrammes/FindDuplicatewordsfromString.java
@@ -5,25 +5,30 @@ public class FindDuplicatewordsfromString
 
 	public static void main(String[] args) 
 	{
-		String s = "Panctuation";
+		String s = "Cirricullum";
 		int len= s.length();
 		int count = 0;
 		int i , j;
 		
 		char[] arr = s.toCharArray();
-		for ( i =0;i< len;i++)
+		for ( i =0;i< len-1;i++)
 		{
-			for ( j =i+1;j<len;j++ )
+			for ( j =i+1;j<len-1;j++ )
 			{
 				if( arr[i]== arr[j])
 				{
 					count++;
 				}
+				
 				 
+			}
+			if(count> 1 )
+			{
+				System.out.println("The duplicate words are "+ arr[i] +" and cont is " + count);
 			}
 			
 		}
-		System.out.println("The duplicate words are"+ arr[i] +" and cont is" + count);
+		
 	}
 
 }
