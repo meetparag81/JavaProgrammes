cf7b24194b4ae8b67ce804845c296935c2cf0850
meetparag81 <paragborawake81@gmail.com>
2018-08-08 00:25:15 +0530

    duplicate character programme

--- JavaProgramme/src/main/java/JavaInterviewProgrammes/FindDuplicatewordsfromString.java
+++ JavaProgramme/src/main/java/JavaInterviewProgrammes/FindDuplicatewordsfromString.java
@@ -1,29 +1,42 @@
 package JavaInterviewProgrammes;
 
+import java.util.HashMap;
+import java.util.Set;
+
 public class FindDuplicatewordsfromString 
 {
 
 	public static void main(String[] args) 
 	{
-		String s = "Panctuation";
-		int len= s.length();
-		int count = 0;
-		int i , j;
-		
-		char[] arr = s.toCharArray();
-		for ( i =0;i< len;i++)
+		String s = "Cirricullum";
+		String result = s.toLowerCase();
+		HashMap<Character, Integer>charwordcount = new HashMap<Character, Integer>();
+		char[]arr = result.toCharArray();// convert string into character
+		//check each character of string array[arr]
+		for(Character c :arr)
 		{
-			for ( j =i+1;j<len;j++ )
+			if(charwordcount.containsKey(c))
 			{
-				if( arr[i]== arr[j])
-				{
-					count++;
-				}
-				 
+				charwordcount.put(c, charwordcount.get(c)+1); //If char is present in charCountMap, incrementing it's count by 1
 			}
+			else
+			{
+				charwordcount.put(c, 1);//If char is not present in charCountMap,putting this char to charwordcount with 1 as it's value
+			}
+		}
+		//Take all keys in one set of character;
+		Set<Character>Keyscharacters = charwordcount.keySet();
+		
+		System.out.println("Duplicate characters in string"+result);
+		for(Character ch:Keyscharacters)
+		if(charwordcount.get(ch)>1) //If any char has a count of more than 1, printing it's count
+		{
+			System.out.println(ch + ":"+charwordcount.get(ch));
 			
 		}
-		System.out.println("The duplicate words are"+ arr[i] +" and cont is" + count);
+		
+		
+		
 	}
 
 }
